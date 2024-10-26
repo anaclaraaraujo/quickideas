7cf56701e8f97202259a8a09464b5c6e3fc54f14
anaclaraaraujo <helloanaclaraaraujo@gmail.com>
2024-10-26 20:59:42 -0300

    feat: create page SignIn

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,13 @@ import theme from './styles/theme'
 import { Home } from './pages/Home'
 import GlobalStyles from './styles/global'
 
+import { SignIn } from './pages/SignIn'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home />
+      <SignIn />
     </ThemeProvider>
   </React.StrictMode>,
 )
