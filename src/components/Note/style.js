16076e32fbaa0e3557985be515f324bcde5e528f
anaclaraaraujo <helloanaclaraaraujo@gmail.com>
2024-10-26 18:59:42 -0300

    fix: correct color scheme

--- src/components/Note/style.js
+++ src/components/Note/style.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const Container = styled.button`
   width: 100%;
-  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_700};
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
 
   border: none;
   border-radius: 1rem;
